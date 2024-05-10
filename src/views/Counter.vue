46c8da717e36fc504ad60243314dd7e4839337e5
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2024-05-10 10:16:46 -0400

    Initial commit

---
+++ src/views/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Counter View</h1>
+  <h2>{{ counter }}</h2>
+
+  <button @click="increase">+1</button>
+  <button @click="decrease">-1</button>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "counter",
+  props: {},
+  emits: [],
+  setup() {
+    const counter = ref(0);
+
+    // const increase = () => {
+    //   counter.value++;
+    // };
+
+    // const decrease = () => {
+    //   counter.value--;
+    // };
+
+    return {
+      counter,
+
+      decrease: () => counter.value--,
+      increase: () => counter.value++,
+    };
+  },
+};
+</script>
+
+<style></style>
